e2a6a5c2754e01da2c7e180ac361ba823d307c7a
PoshX1z <xdante1357@gmail.com>
2026-02-15 15:04:04 +0700

    feat: navbar and assemble into header

--- components/shared/header/Menu.tsx
+++ components/shared/header/Menu.tsx
@@ -1,6 +1,5 @@
 import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
-import React from "react";
 
 const Menu = () => {
   return (
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,25 +3,31 @@ import Image from "next/image";
 import Link from "next/link";
 import { SearchBar } from "./SearchBar";
 import Menu from "./Menu";
+import Navbar from "./Navbar";
 
 export const Header = () => {
   return (
-    <div className="text-6xl flex items-center justify-between">
-      <div className="flex items-center">
-        <Image
-          src="/logo.svg"
-          width={100}
-          height={100}
-          alt="logo"
-          loading="eager"
-        />
-        <Link href="/">{APP_NAME}</Link>
+    <div className="text-6xl">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src="/logo.svg"
+            width={100}
+            height={100}
+            alt="logo"
+            loading="eager"
+          />
+          <Link href="/">{APP_NAME}</Link>
+        </div>
+        <div>
+          <SearchBar />
+        </div>
+        <div>
+          <Menu />
+        </div>
       </div>
       <div>
-        <SearchBar />
-      </div>
-      <div>
-        <Menu />
+        <Navbar />
       </div>
     </div>
   );

1421de5a90aaa2e87681afbc0eab0046465f2005
PoshX1z <xdante1357@gmail.com>
2026-02-15 11:29:52 +0700

    chore: set up codebase

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.APP_NAME || "Final Fantasy";
+export const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION ||
+  "This game store application uses gil currency instead of real money to buy game, user can obtain gil by completing daily quest or after purchasing game";

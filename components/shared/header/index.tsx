d86371127ef3b541f37084aef2944c13b2577a7a
PoshX1z <xdante1357@gmail.com>
2026-02-15 12:27:37 +0700

    chore: set up header

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,24 @@
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { SearchBar } from "./SearchBar";
+
+export const Header = () => {
+  return (
+    <div className="text-6xl">
+      <div className="flex items-center">
+        <Image
+          src="/logo.svg"
+          width={100}
+          height={100}
+          alt="logo"
+          loading="eager"
+        />
+        <Link href="/">{APP_NAME}</Link>
+      </div>
+      <div>
+        <SearchBar />
+      </div>
+    </div>
+  );
+};

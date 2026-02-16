e59cf1f00a713a2fcf2abd3693938368959ae402
PoshX1z <xdante1357@gmail.com>
2026-02-16 19:37:55 +0700

    feat: gil wallet banner

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,20 @@
 import Hero from "@/components/shared/home/Hero";
 import { Promotion } from "@/components/shared/home/Promotion";
+import { Wallet } from "@/components/shared/home/Wallet";
 
 export default function Home() {
   return (
     <div className="pt-5">
       <Hero />
-      <h1 className="py-5 text-8xl">Promotion Games On Sell</h1>
-      <Promotion />
+      <div className="flex">
+        <div>
+          <h1 className="py-5 text-8xl">Promotion Games On Sell</h1>
+          <Promotion />
+        </div>
+        <div className="p-10">
+          <Wallet />
+        </div>
+      </div>
     </div>
   );
 }

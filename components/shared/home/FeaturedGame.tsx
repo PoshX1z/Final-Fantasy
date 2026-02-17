42896fc052c76521a93d5c9765d5681dfb8f3e12
PoshX1z <xdante1357@gmail.com>
2026-02-17 21:37:53 +0700

    chore: database setup #1

--- components/shared/home/FeaturedGame.tsx
+++ components/shared/home/FeaturedGame.tsx
@@ -3,6 +3,7 @@ import ProductCard from "../product/ProductCard";
 export const FeaturedGame = () => {
   return (
     <div>
+      <h1>Featured Games</h1>
       <div className="grid grid-cols-5 gap-5">
         <ProductCard type={2} />
         <ProductCard type={2} />

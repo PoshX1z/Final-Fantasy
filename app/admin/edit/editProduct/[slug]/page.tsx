f2f918522542ebc0f15998403ad4abaffb82ab36
PoshX1z <xdante1357@gmail.com>
2026-03-02 09:26:38 +0700

    chore: admin edit product function #2

--- app/admin/edit/editProduct/[slug]/page.tsx
+++ app/admin/edit/editProduct/[slug]/page.tsx
@@ -1,10 +1,18 @@
 import { EditForm } from "@/components/shared/admin/EditForm";
 import { getProductBySlug } from "@/prisma/actions/product.actions";
 
-export default function EditProductPage() {
+export default async function EditProductPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const product = await getProductBySlug(slug);
   return (
     <div>
-      <EditForm />
+      <h1 className="mb-10 underline">Edit Product {product?.name}</h1>
+      <EditForm key={product?.id} product={product!} />
     </div>
   );
 }
--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -62,3 +62,49 @@ export async function DELETE(req: NextRequest) {
 
   return NextResponse.json({ success: true });
 }
+
+export async function PATCH(req: NextRequest) {
+  const formData = await req.formData();
+  const name = formData.get("name") as string;
+  const slug = formData.get("slug") as string;
+  const image = formData.get("image") as string;
+  const category = formData.get("category") as string;
+  const tag = formData.get("tag") as string;
+  const platform = formData.get("platform") as string;
+  const platformImageIcon = formData.get("platformImageIcon") as string;
+  const edition = formData.get("edition") as string;
+  const description = formData.get("description") as string;
+  const price = Number(formData.get("price"));
+  const countInStock = Number(formData.get("countInStock"));
+  const numSales = Number(formData.get("numSales"));
+  const releaseDate = formData.get("releaseDate") as string;
+  const developer = formData.get("developer") as string;
+  const review = Number(formData.get("review"));
+  const publisher = formData.get("publisher") as string;
+  const delivery = formData.get("delivery") as string;
+  console.log(image);
+  await prisma.product.update({
+    where: {
+      slug: slug,
+    },
+    data: {
+      name: name,
+      image: image,
+      category: category as ProductCategory,
+      tag: tag as ProductTag,
+      platform: platform as ProductPlatform,
+      platformImageIcon: platformImageIcon,
+      edition: edition as ProductEdition,
+      description: description,
+      price: price,
+      countInStock: countInStock,
+      numSales: numSales,
+      releaseDate: releaseDate,
+      developer: developer,
+      review: review,
+      publisher: publisher,
+      delivery: delivery as ProductDeliveryType,
+    },
+  });
+  return NextResponse.json({ success: true });
+}

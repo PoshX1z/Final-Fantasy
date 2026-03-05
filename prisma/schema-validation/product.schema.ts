474df49208040fa21be4e6c66d95a6fced7c88a2
PoshX1z <xdante1357@gmail.com>
2026-03-05 20:22:56 +0700

    chore: update admin to add more than 1 categories

--- prisma/schema-validation/product.schema.ts
+++ prisma/schema-validation/product.schema.ts
@@ -1,47 +1,5 @@
 import z from "zod";
 
-const ProductCategory = [
-  "action",
-  "adventure",
-  "rpg",
-  "jrpg",
-  "strategy",
-  "simulation",
-  "sports",
-  "racing",
-  "fighting",
-  "shooter",
-  "puzzle",
-  "platformer",
-  "survival",
-  "horror",
-  "stealth",
-  "openWorld",
-  "mmo",
-  "battleRoyald",
-  "storyTelling",
-  "rhythm",
-];
-const ProductTag = ["normal", "hero", "promotion", "featured"];
-const ProductPlatform = [
-  "ea",
-  "epic",
-  "microsoft",
-  "nintendo",
-  "playstation",
-  "steam",
-  "ubisoft",
-  "xbox",
-];
-const ProductEdition = [
-  "standard",
-  "deluxe",
-  "ultimate",
-  "premium",
-  "gold",
-  "silver",
-  "platinum",
-];
 const ProductDeliveryType = ["key", "account"];
 export const ProductInputSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 characters"),
@@ -49,11 +7,10 @@ export const ProductInputSchema = z.object({
   description: z.string().min(10, "Description must be at least 10 characters"),
   price: z.coerce.number().positive("Price must be a positive number"),
   image: z.string(),
-  category: z.enum(ProductCategory).default(ProductCategory[0]),
-  tag: z.enum(ProductTag).default(ProductTag[0]),
-  platform: z.enum(ProductPlatform).default(ProductPlatform[0]),
-  platformImageIcon: z.string(),
-  edition: z.enum(ProductEdition).default(ProductEdition[0]),
+  category: z.string(),
+  tag: z.string(),
+  platform: z.string(),
+  edition: z.string(),
   developer: z.string().min(2, "Publisher is required"),
   publisher: z.string().min(2, "Publisher is required"),
   releaseDate: z.string(),
